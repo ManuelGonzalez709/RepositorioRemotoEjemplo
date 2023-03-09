d4e5f07d917d5cca1934e252cfd5ffe17c6e0841
manue <manue@DESKTOP-APF5NIG>
2023-03-09 13:38:24 +0100

    El cambio que he hecho es porque es jueves

---
+++ Proyecto1/src/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * @author manue
+ *
+ */
+module Proyecto1 {
+	
+}
---
+++ Proyecto1/src/paquete1/Principal.java
@@ -0,0 +1,8 @@
+package paquete1;
+
+public class Principal {
+	public static void main(String []agrs) {
+		System.out.println("Estamos muy contentos porque es jueves");
+		
+	}
+}
