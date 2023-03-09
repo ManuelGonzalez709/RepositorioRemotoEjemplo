64778b687cc6a6f3487960d48f157ece904207ef
manue <manue@DESKTOP-APF5NIG>
2023-03-09 13:40:08 +0100

    He cambiado porque mañana es viernes

--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -3,6 +3,6 @@ package paquete1;
 public class Principal {
 	public static void main(String []agrs) {
 		System.out.println("Estamos muy contentos porque es jueves");
-		
+		System.out.println("Mañana estaremos mas contentos porque sera viernes");
 	}
 }
