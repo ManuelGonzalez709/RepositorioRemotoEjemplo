2b9999508bca1e4ce984cbed1faefd0323eb01f1
manue <manue@DESKTOP-APF5NIG>
2023-03-09 13:44:54 +0100

    Hola

--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -4,5 +4,6 @@ public class Principal {
 	public static void main(String []agrs) {
 		System.out.println("Estamos muy contentos porque es jueves");
 		System.out.println("Mañana estaremos mas contentos porque sera viernes");
+		System.out.println("El lunes ya no estaremos tan contentos");
 	}
 }
