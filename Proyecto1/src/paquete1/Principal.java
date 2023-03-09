608db1814974e3e6e68ca31c15112cf54e9440e0
manue <manue@DESKTOP-APF5NIG>
2023-03-09 13:49:11 +0100

    Este programa me esta insultando

--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -5,5 +5,9 @@ public class Principal {
 		System.out.println("Estamos muy contentos porque es jueves");
 		System.out.println("Mañana estaremos mas contentos porque sera viernes");
 		System.out.println("El lunes ya no estaremos tan contentos");
+		
+		for(int i = 0 ; i <= 5 ; i++) {
+			System.out.println("Ijos de puta");
+		}
 	}
 }
